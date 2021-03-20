448cbf64d23e29b035fc74b2b7e48de756638628
histcat <jessejeson@163.com>
2021-03-20 20:26:53 +0800

    update  copy.js.

--- copy.js
+++ copy.js
@@ -1,7 +1,7 @@
 function copy(){
     const input = document.createElement('input');
     document.body.appendChild(input);
-    input.setAttribute('value', "shadowspacex@163.com");
+    input.setAttribute('value', "jessejeson@163.com");
     input.select();
     document.execCommand("copy");
     document.body.removeChild(input);
